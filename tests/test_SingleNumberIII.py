5ce9d301cdde92453f63f792f5676baf98fefc5d
TonyChou <tonychou@wondercise.com>
2023-09-15 18:34:18 +0800

    refactor: 260 SingleNumberIII

--- tests/test_SingleNumberIII.py
+++ tests/test_SingleNumberIII.py
@@ -1,33 +1,17 @@
 from leetcode.SingleNumberIII import Solution
+import pytest
+
+data1 = ([1, 2, 1, 3, 2, 5], [3, 5])
+data2 = ([-1, 0], [0, -1])
+data3 = ([0, 1], [0, 1])
+data4 = ([1, 1, 3, 3, 5, 6], [5, 6])
+data5 = ([-283, -174, 44, -174, 40, 40, 101, 101, 354, -283, 513, 513], [354, 44])
 
 
 class TestSolution:
-    def test_singleNumber1(self):
-        nums = [1, 2, 1, 3, 2, 5]
-        s = Solution()
-        result = s.singleNumber(nums)
-        assert result == [3, 5] or result == [5, 3]
 
-    def test_singleNumber2(self):
-        nums = [-1, 0]
+    @pytest.mark.parametrize("nums, expected", [data1, data2, data3, data4, data5])
+    def test_singleNumber(self, nums, expected):
         s = Solution()
         result = s.singleNumber(nums)
-        assert result == [-1, 0] or result == [0, -1]
-
-    def test_singleNumber3(self):
-        nums = [0, 1]
-        s = Solution()
-        result = s.singleNumber(nums)
-        assert result == [0, 1] or result == [1, 0]
-
-    def test_singleNumber4(self):
-        nums = [1, 1, 3, 3, 5, 6]
-        s = Solution()
-        result = s.singleNumber(nums)
-        assert result == [5, 6] or result == [6, 5]
-
-    def test_singleNumber5(self):
-        nums = [-283, -174, 44, -174, 40, 40, 101, 101, 354, -283, 513, 513]
-        s = Solution()
-        result = s.singleNumber(nums)
-        assert result == [44, 354] or result == [354, 44]
+        assert result == expected
